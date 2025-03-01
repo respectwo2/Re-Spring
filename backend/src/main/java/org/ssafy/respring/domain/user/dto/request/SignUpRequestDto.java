a9fc74d3a52ec721f1209b11cd9b989e44caa20d
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2025-03-01 17:07:16 +0900

    feat: 회원가입 유효성 검사 추가

--- backend/src/main/java/org/ssafy/respring/domain/user/dto/request/SignUpRequestDto.java
+++ backend/src/main/java/org/ssafy/respring/domain/user/dto/request/SignUpRequestDto.java
@@ -10,8 +10,17 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto {
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    @Size(min = 2, message = "사용자 이름은 2자 이상이어야 합니다.")
     private String userNickname;
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
+
     private String provider;
 }
